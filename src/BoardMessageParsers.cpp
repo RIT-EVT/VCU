67dc6235becb432258ec88d993eb42ae229affdb
tmb5932 <tmb5932@g.rit.edu>
2026-01-29 22:36:19 -0500

    updated raw can decryption of data

--- src/BoardMessageParsers.cpp
+++ src/BoardMessageParsers.cpp
@@ -10,25 +10,24 @@ BMSParsed parseBMSMessage(io::CANMessage& message) {
 }
 
 HIBParsed parseHIBMessage(io::CANMessage& message) {
-    // TODO: HIB example implementation, update when HIB is completed
     uint8_t* payload  = message.getPayload();
-    uint16_t throttle = (payload[0]);
-    throttle <<= 8;
-    throttle += (payload[1]);
+    uint16_t throttle = (payload[0] << 8) | payload[1];
 
-    bool forwardEnable   = ((payload[2] & 0b10000000) != 0);
-    bool startPressed    = ((payload[2] & 0b01000000) != 0);
-    bool brakeOn         = ((payload[2] & 0b00100000) != 0);
-    bool comparisonFault = ((payload[2] & 0b00010000) != 0);
+    uint16_t brakeMillivolts = (payload[2] << 8) | payload[3];  // 0 to 12_000 (millivolts)
+    bool brakeOn         = brakeMillivolts > 6000;  // Completely magical number decided by EE's
+
+    bool comparisonFault = ((payload[4] & 0b00000100) | (payload[5] & 0b00000100)) != 0;
+    bool forwardEnable   = (payload[6] & 0b00000001) != 0;  // todo: double check these once HIB code is merged
+    bool startPressed    = (payload[6] & 0b00000010) != 0;
 
     return HIBParsed{static_cast<int16_t>(throttle), forwardEnable,
                      startPressed, brakeOn, comparisonFault};
 }
 
 GFDBParsed parseGFDBMessage(io::CANMessage& message) {
-    // TODO: GFDB example implementation, update when GFDB is completed
-    uint8_t* payload     = message.getPayload();
-    bool isolationState = payload[0];
+    // Based on SIM200 CAN Documentation
+    uint8_t* payload       = message.getPayload();
+    uint8_t isolationState = payload[1];
     return GFDBParsed{isolationState};
 }
 
