43ee73476c5c62d23e0873351b699ad40409c73e
tmb5932 <tmb5932@g.rit.edu>
2026-06-22 22:28:35 -0400

    lying pieces of garbage said active low estop...

--- src/MCuC.cpp
+++ src/MCuC.cpp
@@ -227,8 +227,8 @@ void MCuC::process(core::rtos::EventFlags* flags) {
 //    modelInputs.InterlockB       = gpios.interlockBGPIO.readPin() == io::GPIO::State::HIGH;
     modelInputs.Ignition_LS_A     = gpios.ignitionAGPIO.readPin() == io::GPIO::State::LOW; // active low
     modelInputs.Ignition_LS_B     = gpios.ignitionBGPIO.readPin() == io::GPIO::State::LOW; // active low
-    modelInputs.ESTOP_LS_A        = gpios.eStopAGPIO.readPin() == io::GPIO::State::LOW;    // active low
-    modelInputs.ESTOP_LS_B        = gpios.eStopBGPIO.readPin() == io::GPIO::State::LOW;    // active low
+    modelInputs.ESTOP_LS_A        = gpios.eStopAGPIO.readPin() == io::GPIO::State::HIGH;
+    modelInputs.ESTOP_LS_B        = gpios.eStopBGPIO.readPin() == io::GPIO::State::HIGH;
 
     // Set CAN inputs (values updated over CAN)
 
